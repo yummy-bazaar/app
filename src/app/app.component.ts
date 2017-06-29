7b96579d1fae164867fad7d5d9b322fa52418a43
Randy Champagne <r-champagne@neiu.edu>
2017-06-29 12:59:48 -0400

    reslved version clash between typescript m rxjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,24 @@ import { Component } from '@angular/core';
 	template: `<h1>Hello {{backend}} from {{frontend}}</h1>`,
 })
 export class AppComponent  { 
-	backend = 'Shopify'; 
-	frontend = 'Angular';
+	backend 	= 'Shopify'; 
+	frontend 	= 'Angular';
+
+	shopClient 	= require('shopify-buy').buildClient({
+		accessToken: 	'9b5e591536c4538e6299c2981dec9486',
+		domain: 		'yummy-bazaar-dev.myshopify.com',
+		appId: 			'6'
+	});
+
+
+	/*
+	shopClient
+		.fetchAllProducts()
+		.then(
+			console.log
+		)
+		.catch( (e) => {
+			console.log('Request failed',e);
+		});
+	*/
 }
