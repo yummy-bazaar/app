1a6df5df5a3d2f5563ee8ce04009cdb727833601
Randy Champagne <r-champagne@neiu.edu>
2017-06-30 10:18:25 -0400

    outlined algo for brand service

--- src/app/brand.service.ts
+++ src/app/brand.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } 		from '@angular/core';
 
 import { Brand } 			from './brand';
+import { Product }			from './product';
 import client 				from './backend-client';	// TODO: replace this service with the shopify buy client
 import { Logger } 			from './logger.service';
 
@@ -11,63 +12,24 @@ export class BrandService {                              // TODO: change this to
 	private Brands: Brand[] = [];						// TODO: use this to cache Brands collection
 
 	constructor(
-		private client: client,				// TODO: replace this service with the shopify buy client
 		private logger: Logger
 	) { }
 
 	getBrands() {
-		this.client.getAll(Brand).then( (Brands: Brand[]) => {
-		this.logger.log(`Fetched ${Brands.length} Brands.`);
-		this.Brands.push(...Brands); 					// TODO: fill cache
-	});
+		client
+			// TODO: can I use GraphQL to compose Brand collection from the server?
+			.fetchAllProducts()
+			.then( (products: Product[]) => {
+				this.logger.log(`Fetched ${products.length} Products.`);
 
-	return this.Brands;
-  }
+				// TODO: fill cache
+				//			- transform products collection into brand collection
+				//			- push new array into cache
+				//this.Brands.push(...Brands); 					
+			});
+
+		return this.Brands;
+	}
 }
 
 
-/*
-
-
-// import client
-import client 	from './shopify-buy-client';
-
-
-
-// fetch shop metadata
-const shopPromise 		= client.fetchShopInfo();
-// fetch data for entire product catalog
-const productsPromise 	= client.fetchAllProducts();
-
-
-// consume promises
-Promise
-	.all(
-		[
-			shopPromise,
-			productsPromise, 
-		]
-	)
-	.then(
-		([shop, products]) => {
-
-   			// log shop data
-   			console.log(
-   							'shop is: ', 
-   							JSON.stringify(shop,null,4)
-   			);
-
-
-			// log product data
-   			console.log(
-   							'products are: ', 
-   							JSON.stringify(products,null,4)
-   			);
-    	}
-    )
-    .catch((err) => console.error(err));
-
-
-
-
-*/
