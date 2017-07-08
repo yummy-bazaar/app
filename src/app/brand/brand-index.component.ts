2221ac8b46429162850112be75e50e2a974f4b64
Randy Champagne <r-champagne@neiu.edu>
2017-07-08 01:38:15 -0400

    added more next steps

--- src/app/brand/brand-index.component.ts
+++ src/app/brand/brand-index.component.ts
@@ -94,11 +94,15 @@ export class BrandIndexComponent implements OnInit {
 	// - Use RxJS so I only have to query the backend once
 	// - will this give me the progressive SPA ux?
 	// - see: http://dev.apollodata.com/angular2/queries.html#rxjs
+	// - see: http://dev.apollodata.com/angular2/typescript.html
 	// TODO:
-	// - implement pagination | infinite scroll to reduce round-trip time
+	// - impl pagination | infinite scroll to reduce round-trip time
 	// - this is prolly not as useful in the index view cause I need entire brand catalog
 	// - this will be useful in the brand view
 	// - see: http://dev.apollodata.com/angular2/pagination.html
+	// TODO:
+	// - impl logic to update client side cache whenever catalog is updated on backend
+	// - see: http://dev.apollodata.com/angular2/receiving-updates.html
 	private init() {
 
 		this.data = this.client
