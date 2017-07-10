8f4eb504969494c492850380377c53f89536214d
Randy Champagne <r-champagne@neiu.edu>
2017-07-10 09:46:37 -0400

    disabled brands ng module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,13 @@ import { Component } 	from '@angular/core';
 
 @Component({
 	selector: 'angular-test',
+	template:``,
+	/*
 	template: `
 		<h1>Hello {{backend}} from {{frontend}}</h1>
 		<brands></brands>
 	`,
+	*/
 })
 export class AppComponent  { 
 	backend 	= 'Shopify'; 
