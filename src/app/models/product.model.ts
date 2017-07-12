b43404d0de7a24fc738c238dfe847495689e057d
Randy Champagne <r-champagne@neiu.edu>
2017-07-12 10:59:24 -0400

    added next steps

--- src/app/models/product.model.ts
+++ src/app/models/product.model.ts
@@ -4,3 +4,23 @@ export interface Product {
         vendor: string
     }
 }
+
+/*
+
+
+
+Brand data model:
+- name
+- description
+- cover image
+- 1-M Product (a collection of products)
+
+
+Product data model:
+- name
+- slug
+- description
+- cover image
+- (price, inventory status...)
+
+*/
