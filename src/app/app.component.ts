5e8f6fc5abcd3d0246791ff2519c969ef34c6a80
Randy Champagne <r-champagne@neiu.edu>
2017-08-03 00:06:46 -0400

    re-enabled angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,8 @@
 import { Component } 	from '@angular/core';
 
 @Component({
-	selector: 'angular-test',
-	template:``,
-	/*
-	template: `
-		<h1>Hello {{backend}} from {{frontend}}</h1>
-		<brands></brands>
-	`,
-	*/
+	selector: 'app-root',
+	template: require('./app.component.html')
 })
 export class AppComponent  { 
 	backend 	= 'Shopify'; 
