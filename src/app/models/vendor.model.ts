11cc508718e9ab16bc16fa86909344793a172ab3
Randy Champagne <r-champagne@neiu.edu>
2017-08-03 02:22:31 -0400

    init vendor model

---
+++ src/app/models/vendor.model.ts
@@ -0,0 +1,26 @@
+export interface Vendor {
+    node: {
+    	//id: string;
+        vendor: string
+    }
+}
+
+/*
+
+
+
+Brand data model:
+- name
+- description
+- cover image
+- 1-M Product (a collection of products)
+
+
+Product data model:
+- name
+- slug
+- description
+- cover image
+- (price, inventory status...)
+
+*/
