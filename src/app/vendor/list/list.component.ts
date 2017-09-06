144290ced83393ea261e2bd015c26ce5975396d7
Randy Champagne <r-champagne@neiu.edu>
2017-09-06 11:10:12 -0400

    impl preview component

--- src/app/vendor/list/list.component.ts
+++ src/app/vendor/list/list.component.ts
@@ -1,19 +1,7 @@
 import { 
 	Component,
-	EventEmitter,
-	Input,
-	Output,
-	OnInit,
-	OnDestroy
+	Input
 }					 		from '@angular/core';
-import { 
-	LoggerService
-}							from '../../utils';
-
-
-
-
-
 
 
 
@@ -23,38 +11,12 @@ import {
 	templateUrl: './list.component.html',
 	styleUrls: 	 ['./list.component.styl']
 })
-export class ListComponent implements OnInit, OnDestroy {
+export class ListComponent {
 	
 
-	//private newKey:				string;
 	@Input() selectedVendors:	Set<any>;
 	
-
 	
-	constructor(
-		private logger: LoggerService
-	) {}; 
-
-
-
-	ngOnInit(): void {
-
-		// Debug
-		this.logger.log('Starting ListComponent.ngOnInit()');
-
-		// Debug
-		this.logger.log('Completed ListComponent.ngOnInit()');
-	};
-
-
-
-	ngOnDestroy(): void {
-
-		// Debug
-		this.logger.log('Starting ListComponent.ngOnDestroy()');
-
-		// Debug
-		this.logger.log('Completed ListComponent.ngOnDestroy()');
-	}
+	constructor() {}; 
 
 }
---
+++ src/app/vendor/preview/preview.component.ts
@@ -0,0 +1,132 @@
+// NG core modules
+import { 
+	Component,
+	Input, 
+	OnInit
+} 							from '@angular/core';
+
+// 3rd party modules
+import {
+	ApolloQueryObservable
+}							from 'apollo-angular';
+
+// Project Services
+import {
+	GraphQLService,
+	ProductsQuery,
+	ProductsUpdateQuery
+}							from '../../graphql';
+import{
+	LoggerService
+} 							from '../../utils';
+
+
+
+
+@Component({
+  selector: 	'vendor-preview',
+  templateUrl: 	'./preview.component.html',
+  styleUrls: 	['./preview.component.styl']
+})
+export class PreviewComponent implements OnInit {
+
+	
+	private dataStream: 	ApolloQueryObservable<any>;
+	_vendorId:			string;
+
+	@Input() 
+	public set vendorId(id: string) {
+		this._vendorId = id;
+		this.fetch(id);
+	}
+
+	products:					any;
+	private query:				any;
+	private updateQuery:		any;
+	private offset:				string;
+	private limit:				number;
+	private path2FetchMoreFlag: string;
+	private path2Object:		string;
+
+
+
+	constructor(
+		private service: 	GraphQLService,
+		private logger:		LoggerService
+	) { 
+
+		this.query 				= ProductsQuery;
+		this.updateQuery		= ProductsUpdateQuery;
+		this.offset				= null;
+		this.limit				= 3;
+		this.path2FetchMoreFlag = 'data.shop.products.pageInfo.hasNextPage';
+		this.path2Object 		= 'data.shop.products.edges';
+		this.products 			= [];
+	}
+
+
+
+	ngOnInit() {
+	}
+
+
+
+	fetch(vendorId: string) {
+		// run query
+		this.dataStream = this.service.fetch(
+			this.query,
+			this.offset,
+			this.limit,
+			`title=${vendorId}`,
+			this.path2FetchMoreFlag,
+			this.path2Object,
+			this.updateQuery
+		);
+
+
+		// subscribe to stream
+		this.dataStream.subscribe(
+			({data, loading}) => {
+
+				// Debug
+				this.logger.log('Starting to consume API payload in ProductComponent.fetch()');
+				//this.logger.warn(JSON.stringify(data,null,4));
+
+
+				// parse product data
+				try {
+
+					// take first 3 products
+					for (let i=0; i<3; i++){
+
+						// parse single product
+						let product = {
+							id: data.shop.products.edges[i].node.title,
+							img: {
+								src: data.shop.products.edges[i].node.images.edges[0].node.src,
+							}
+						}
+
+						// push product in collection
+						this.products.push(product);
+					}
+
+					
+				}
+				catch(e) {
+					this.logger.warn(`error parsing product data`);
+					this.logger.warn(JSON.stringify(e,null,4));
+				}
+
+
+				// Debug
+				this.logger.log('Finished consuming API payload in ProductComponent.fetch()');
+			},
+			(err) => { 
+				this.logger.error('Fetch error: ' + err.message); 
+			}
+		)
+
+	}
+
+}
