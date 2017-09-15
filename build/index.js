e433326dc4e39579d2863de889f98f882d9f1b3d
Randy Champagne <r-champagne@neiu.edu>
2017-09-15 09:01:03 -0400

    import Shopify theme n build scripts

---
+++ build/index.js
@@ -0,0 +1,23 @@
+export { copyTheme }	from './copyTheme';
+export { copyLibs }		from './copyLibs';
+export { lintScripts } 	from './lintScripts';
+export { buildScripts }	from './buildScripts';
+export { watchDirs }	from './watchDirs';
+export { paths }		from './paths';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
