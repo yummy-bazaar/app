66aba52f135d433b2bc4377c24f9755589c93d28
Randy Champagne <r-champagne@neiu.edu>
2017-09-15 09:28:12 -0400

    installed jquery n bootstrap as dependencies

--- src/main.ts
+++ src/main.ts
@@ -12,8 +12,39 @@ import {
 	environment 
 } 							from './environments/environment';
 
+
+
 if (environment.production) {
 	enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
+
+
+
+
+
+// service worker config
+if ('serviceWorker' in navigator) {
+	navigator
+		.serviceWorker
+		.register('/service-worker.js')
+		.then(function(registration) {
+			console.log('Service Worker registered');
+		})
+		.catch(function(err) {
+			console.log('Service Worker registration failed: ', err);
+		})
+	;
+}
+	
+
+
+
+
+
+
+
+
+
+
